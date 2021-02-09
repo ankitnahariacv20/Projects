3da48b696587d64f3d4eeaf2206bd3ba51f7e4b0
ankitnahariacv20 <78737335+ankitnahariacv20@users.noreply.github.com>
2021-02-09 10:08:09 +0530

    Add files via upload

---
+++ sum of first n odd .c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+int squares(int);
+int main()
+{
+    int n;
+    printf("Enter the value of n\n");
+    scanf("%d", &n);
+
+    squares(n);
+    getch();
+    return 0;
+}
+int squares(int a)
+{
+    int i=1;
+    float sum=0;
+    while(i<=a)
+    {
+        if(i%2!= 0)
+        {
+            sum=sum+i*i;
+        }
+        i++;
+    }
+    printf("The sum of squares of odd numbers upto %d is %f ",a,+sum);
+    getch();
+    return 0;
+}
